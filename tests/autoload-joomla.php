5d9bb3320d22e56b66f40fb13e3b441c48b74d10
SmetDenis <denis@jbzoo.com>
2015-12-15 17:34:10 +0600

    copyrights

--- tests/autoload-joomla.php
+++ tests/autoload-joomla.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * JBZoo CrossCMS
+ *
+ * This file is part of the JBZoo CCK package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package   CrossCMS
+ * @license   MIT
+ * @copyright Copyright (C) JBZoo.com,  All rights reserved.
+ * @link      https://github.com/JBZoo/CrossCMS
+ */
 
 define('_JEXEC', 1); // We are a valid entry point.
 define('JPATH_BASE', CMS_JOOMLA_PATH);
