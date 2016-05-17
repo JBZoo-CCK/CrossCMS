b822cca559f5359488e15aa88e35ee5aa245ba5e
SmetDenis <denis@jbzoo.com>
2016-05-17 13:37:59 +0700

    sh -> bash

--- tests/bin/prepare.sh
+++ tests/bin/prepare.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 sudo apt-get update -qq
 sudo apt-get install -y -qq postfix
@@ -7,4 +7,3 @@ sudo service postfix stop
 smtp-sink -d "%d.%H.%M.%S" 127.0.0.1:2500 1000 &
 echo -e '#!/usr/bin/env bash\nexit 0' | sudo tee /usr/sbin/sendmail
 echo 'sendmail_path = "/usr/sbin/sendmail -t -i "' | sudo tee "/home/travis/.phpenv/versions/`php -i | grep "PHP Version" | head -n 1 | grep -o -P '\d+\.\d+\.\d+.*'`/etc/conf.d/sendmail.ini"
-
