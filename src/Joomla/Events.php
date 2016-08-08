6b8b686d1df5a5b2ed711223e4543d9c8bc7a837
Denis Smetannikov <SmetDenis@users.noreply.github.com>
2016-08-08 22:46:19 +0700

    Rename event name

--- src/Joomla/Events.php
+++ src/Joomla/Events.php
@@ -51,7 +51,7 @@ class Events extends AbstractEvents
      *      }
      *
      *      // Content handlers (for macroses)
-     *      public function onAfterRespond()
+     *      public function onAfterRender()
      *      {
      *          $body = JFactory::getApplication()->getBody();
      *          $this->_app->trigger(AbstractEvent::EVENT_CONTENT, [$body]);
